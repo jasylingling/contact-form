2417db2190bc8a94e6fe707d1e301441dff8a1a0
Jasmin Fischli <jasmin@smartive.ch>
2023-11-01 09:55:08 +0000

    feat: set up project structure and contact form data binding

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-// import { useState } from 'react';
-// import reactLogo from './assets/react.svg';
-// import viteLogo from '/vite.svg';
-import './App.css';
+import FormHeader from './components/FormHeader';
+import ContactForm from './components/ContactForm';
 
 function App() {
-  // const [count, setCount] = useState(0);
-
   return (
-    <h1 className="text-3xl font-bold underline">Contact form</h1>
+    <main>
+      <FormHeader subheading="Let's get in touch!">
+        Contact
+      </FormHeader>
+      <ContactForm></ContactForm>
+    </main>
   );
 }
 
